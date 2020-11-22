f5bd2e7147f63e12883aba6e2542ee691ab1c6a2
Muhammad Andika <48826191+andikadayu@users.noreply.github.com>
2020-11-22 19:04:24 +0700

    Update Modal Form Report

--- resources/views/pages/monitoring.blade.php
+++ resources/views/pages/monitoring.blade.php
@@ -58,7 +58,7 @@
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Report Jurnal</h4>
+                <h4 class="modal-title">Report Monitoring</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
